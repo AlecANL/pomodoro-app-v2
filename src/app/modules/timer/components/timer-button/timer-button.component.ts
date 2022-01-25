08dc0daf95dc3bee93119bdaf09b0a3ce5f64dff
alexei <alexeialvarez4@gmail.com>
2022-01-24 21:50:55 -0600

    :fire: feat(timer): add basic timer functionality :green_heart:

--- src/app/modules/timer/components/timer-button/timer-button.component.ts
+++ src/app/modules/timer/components/timer-button/timer-button.component.ts
@@ -1,12 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'timer-button',
   templateUrl: './timer-button.component.html',
   styleUrls: ['./timer-button.component.css'],
 })
-export class TimerButtonComponent implements OnInit {
-  constructor() {}
+export class TimerButtonComponent implements OnInit, OnDestroy {
+  time: number = 2 * 60;
+  timing!: ReturnType<typeof setInterval>;
+  togglePlay: boolean = false;
 
   ngOnInit(): void {}
+
+  constructor() {}
+
+  toggle() {
+    this.togglePlay = !this.togglePlay;
+    console.log(this.togglePlay ? 'paused' : 'play');
+    !this.togglePlay ? this.pauseTimer() : this.handleTiming();
+  }
+
+  handleTiming() {
+    this.timing = setInterval(() => {
+      this.time = this.time - 1;
+    }, 1000);
+  }
+
+  present(value: number) {
+    return `${('0' + Math.floor(value / 60)).slice(-2)}:${(
+      '0' + Math.floor(value % 60)
+    ).slice(-2)}`;
+  }
+
+  pauseTimer() {
+    clearInterval(this.timing);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timing);
+  }
 }
